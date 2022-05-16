6b9aa73ceae9083cdea230109765c7952312ab29
Neko Of The Abyss <thenekooftheabyss@gmail.com>
2022-05-16 23:12:18 +0530

    doc

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # number-wizard
 A bunch of functions to convert numbers into strings of required format.
+
+## How to run
+```js
+import {AddComma, AddZero} from "https://deno.land/x/numwizard@v1.0.0/mod.ts"
+
+// Add Commas
+AddComma(123456789) // "123,456,789"
+AddComma(123456789, "in") // "12,34,56,789"
+
+// Add Zeros
+AddZero(1234, 9) // "000001234"
+```
--- src/add_comma.ts
+++ src/add_comma.ts
@@ -2,7 +2,7 @@
 
 /**
  * @param {number} x - Number to add commas in.
- * @param {string} type - Specify "in" for Indian system. Leave blank for the general one.
+ * @param {string} type - Specify "in" for Indian system. Specify "ja" for Japanese system (the literal one or so I guess). Leave blank for the general one.
  */
 export default (x: number, type?: string): string => {
   if(!type) type = "x"
@@ -13,6 +13,11 @@ export default (x: number, type?: string): string => {
       y[0] = Math.trunc(Number(y[0]) / 1000).toString()
       .replace(/\B(?=(\d{2})+(?!\d))/g, ",");
       y[0] = `${y[0]},${thousand}`
+      return y.join(".");
+    }
+    case "ja": {
+      const y = x ? x.toString().split(".") : [""];
+      y[0] = y[0].replace(/\B(?=(\d{4})+(?!\d))/g, ",");
       return y.join(".");
     }
     default: {
