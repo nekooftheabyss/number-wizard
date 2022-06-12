66010fff89842f221399b3845c08a813af34ccca
Neko Of The Abyss <psi@innocent.com>
2022-06-12 22:47:58 +0530

    don't export words

--- src/mod.ts
+++ src/mod.ts
@@ -2,4 +2,4 @@ export { default as AddComma } from "./add_comma.ts";
 export { default as AddZero } from "./add_zero.ts";
 export { default as RemoveRecurring } from "./remove_recurring.ts";
 export { default as Rearrange } from "./rearrange.ts";
-export { default as InWords } from "./in_words.ts"
+// export { default as InWords } from "./in_words.ts"
